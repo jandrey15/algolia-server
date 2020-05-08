508a0d6dc721b6718e8912b7a61a6aa26d7fde1d
John Andrey <andrey_201015@hotmail.com>
2020-05-08 11:39:29 -0500

    Change client index algolia

--- src/algolia.js
+++ src/algolia.js
@@ -2,6 +2,6 @@ const algoliasearch = require('algoliasearch/lite')
 const { config } = require('./config')
 
 const client = algoliasearch('ZKVB80BMVH', config.apiKeyAdmin)
-const index = client.initIndex('BlogSearch')
+const index = client.initIndex('instant_search_prod')
 
 module.exports = index
